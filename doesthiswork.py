86af7511f8f754b5d00326ea70ac4fb7dcf16336
Robonglious <47901663+Robonglious@users.noreply.github.com>
2023-04-28 09:00:30 -0700

    broken...

---
+++ doesthiswork.py
@@ -0,0 +1,12 @@
+from bark import SAMPLE_RATE, generate_audio, preload_models
+from IPython.display import Audio
+
+# download and load all models
+preload_models()
+
+# generate audio from text
+text_prompt = "Hello, world!"
+audio_array = generate_audio(text_prompt)
+
+# play text in notebook
+Audio(audio_array, rate=SAMPLE_RATE)
